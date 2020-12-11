85630d87689b4911ca66177af995fc45a91211e5
FayezMohtaseb <fayez1997@gmail.com>
2020-12-11 17:22:15 +0200

    First commit

---
+++ Information_Retrieval.py
@@ -0,0 +1,30 @@
+
+
+def load_dataset():
+    pass
+
+
+def get_image():
+    pass
+
+
+def resize_image():
+    pass
+
+
+def get_histogram():
+    pass
+
+
+def save_histograms():
+    pass
+
+
+def load_histograms():
+    pass
+
+
+if __name__ == '__main__':
+    pass
+
+
