73ba31845f25c86ea903d7f13f6f7c7c0a05c99d
islam <161100@ppu.edu.ps>
2020-12-24 17:31:22 +0200

    adding important library for image retrieval

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import Information_Retrieval as IR
 from PIL import Image
+from tkinter.filedialog import askopenfilename
+from PIL import Image,ImageTk
+from tkinter import messagebox
+import os
+try:
+    # Python2
+    import Tkinter as tk
+except ImportError:
+    # Python3
+    import tkinter as tk
 
 img_path = input('Enter Image Full Path: ')
 input_img = Image.open(img_path)
@@ -7,3 +17,10 @@ images = IR.get_similar(input_img)
 for img_name in images:
     img = IR.get_image(img_name)
     img.show()
+    
+    
+    
+    
+    
+    
+     
