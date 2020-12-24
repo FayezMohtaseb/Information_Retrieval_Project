aac8bf18ef19ecabf7ff6cb3b33c73279737aa38
islam <161100@ppu.edu.ps>
2020-12-24 17:33:22 +0200

    adding message box dialog

--- main.py
+++ main.py
@@ -11,14 +11,30 @@ except ImportError:
     # Python3
     import tkinter as tk
 
-img_path = input('Enter Image Full Path: ')
-input_img = Image.open(img_path)
-images = IR.get_similar(input_img)
-for img_name in images:
-    img = IR.get_image(img_name)
-    img.show()
+
+window= tk.Tk()
+window.eval('tk::PlaceWindow %s center' % window.winfo_toplevel())
+window.withdraw()
+if messagebox.askyesno('Question'," choose yes if you want to test image retival program otherise click no")==True:
+    window.deiconify()
+    window.destroy()
+    window.quit()
+    root = tk.Tk()
+    root.withdraw()
+    root.update()
+    # get a series of gif images you have in the working folder
+    # or use full path, or set directory to where the images are
+    image_files = []
     
-    
+
+# img_path = input('Enter Image Full Path: ')
+# input_img = Image.open(img_path)
+# images = IR.get_similar(input_img)
+# for img_name in images:
+#     img = IR.get_image(img_name)
+#     img.show()
+#
+#
     
     
     
